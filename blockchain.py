50b4710f2cb4829c1819eb84ddb67bfe285758fe
dscholefield <david@port80.com>
2022-03-22 09:42:13 +0000

    Developed block class (in progress)

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,27 @@
 
+from operator import truediv
+import hashlib 
+
+class BlockNotValid(Exception):
+    def __init__(self, message):
+        self.message = message
+
 class Peer:
     def __init__(self, name: str):
         self.name = name
+        self.last_block = None
+        self.block_list = None
+
+    def add_block(self, block: Block):
+        pass
 
 
-class BlockChain:
+
+
+class BlockChainConfig:
     """
-    A class to represent a specific blockchain impmentation
+    A class to represent a specific blockchain implementation config
+    (not a blockchain itself)
     
     ...
     
@@ -49,6 +64,104 @@ class BlockChain:
         for peer in self.peers:
             name_list.append(peer.name)
         return name_list
+
+
+
+class Block:
+    """
+    This is the main class to define an individual block in a blockchain
+
+    ...
+
+    Attributes
+    ----------
+
+    config : BlockChainConfig
+        the current config for this blockchain
+    block_number : int
+        the unique number (in a chain) for this block, starting at 0 for first block
+    previous_hash : str
+        the hash of the previous block (or 'None' for block number 0)
+    _nonce : int
+        the mined integer to create the hash with the relevant difficulty (from BlockChainConfig.difficulty)
+    this_hash : str
+        the hash for this block once mined
+    content : str
+        any string content desired (or None if not yet added)
+    _is_mined : bool
+        indicates whether the this_hash has a valid hash for the content
+
+    Methods
+    -------
+    check -> bool
+        checks that the block is mined and valid by hashing the relevant fields and
+        comparing the resulting hash with the difficulty requirement
+
+    has_zeros(hash: str, count: int) -> bool
+        confirms that the given hash has at least 'count' number of trailing zeros
+        which is used to confirm that the difficulty level is met
+
+    """
+
+    def __init__(self, config: BlockChainConfig, block_number: int, previous_hash: str) -> None:
+        self.config = config
+        self.block_number = block_number
+        self.previous_hash = previous_hash
+        self.nonce = None
+        self.this_hash = None
+        self.content = None
+        self._is_mined = False
+
+    # confirm whether last few characters are zeros or not
+    @staticmethod
+    def has_zeros(hash: str, count: int) -> bool:
+        # expecting a sha256 hash which is 32 chars
+        last_char = -1
+        matched = True
+        first_char = last_char - count 
+        for pos in range(-1,first_char,-1):
+                if hash[pos] != "0":
+                        matched = False
+        return matched
+
+    # create a hash of this block
+    def block_hash(self) -> str:
+        content_to_hash = str(self.block_number) \
+                            + ':' + self.previous_hash \
+                            + ':' + str(self.nonce) \
+                            + ':' + self.content
+        hash = hashlib.sha256(content_to_hash.encode())
+
+    # check that the claimed hash in the block is the actual hash
+    # of the content and other required fields and that it has
+    # the correct minimum level of difficulty
+    def check(self) -> bool:
+        hash = hashlib.sha256(self.block_hash())
+        return (hash == self.this_hash) and (has_zeros(hash, self.config.difficulty))
+
+
+    @property
+    def is_mined(self):
+        return self._is_mined
+
+    @is_mined.setter
+    def is_mined(self, flag):
+        # if the block becomes 'unmined' then the nonce is cleared
+        # and the hash for this block is cleared
+        if not flag:
+            self.nonce = None
+            self.this_hash = None
+            self._is_mined = False
+        else:
+            # we're setting this block to 'is mined' so we need
+            # to check it
+            if self.check():
+                self._is_mined = True
+            else:
+                BlockNotValid("block hash is not correct - not mined")
+
+
+
 
 
 peer1 = Peer("peer 1")
